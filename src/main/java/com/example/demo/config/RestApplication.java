73b92a5efeafdf2d21448164bcad1cd02d24a2cc
Sebastian <sebasar751@gmail.com>
2020-11-28 17:03:29 -0500

    Ajuste para desplegar en Heroku

--- src/main/java/com/example/demo/config/RestApplication.java
+++ src/main/java/com/example/demo/config/RestApplication.java
@@ -1,14 +1,14 @@
-package com.example.demo.config;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
- 
-@SpringBootApplication
-public class RestApplication implements WebMvcConfigurer{
- 
-    public static void main(String[] args) {
-        SpringApplication.run(RestApplication.class, args);
-    }
-     
-}
+//package com.example.demo.config;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// 
+//@SpringBootApplication
+//public class RestApplication implements WebMvcConfigurer{
+// 
+//    public static void main(String[] args) {
+//        SpringApplication.run(RestApplication.class, args);
+//    }
+//     
+//}
