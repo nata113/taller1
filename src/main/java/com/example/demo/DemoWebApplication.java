24ea1dab105afbc6a2310922db5eedfdb52992cb
Sebastian <sebasar751@gmail.com>
2020-11-28 17:24:30 -0500

    Configuraciones

--- src/main/java/com/example/demo/DemoWebApplication.java
+++ src/main/java/com/example/demo/DemoWebApplication.java
@@ -3,11 +3,19 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 @EnableConfigServer
 public class DemoWebApplication {
 
+	@GetMapping("/")
+	public String desplegado() {
+		return "Se ha desplegado la app";
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DemoWebApplication.class, args);
 	}
