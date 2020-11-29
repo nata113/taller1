0cd5ef146114951b01ed9da714bb28f3006bc726
Sebastian <sebasar751@gmail.com>
2020-11-29 11:09:07 -0500

    Se configura servidor eureka

--- src/main/java/com/taller/core/Taller1Application.java
+++ src/main/java/com/taller/core/Taller1Application.java
@@ -2,6 +2,7 @@ package com.taller.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableConfigServer
+@EnableDiscoveryClient
 public class Taller1Application {
 
 	@GetMapping("/")
